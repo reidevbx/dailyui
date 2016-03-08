2aacbc662571d3998357d462b57e6cca962f56ec
Rplus <rplus.tw@gmail.com>
2016-03-08 11:22:24 +0800

    fix coding style

--- js/main.js
+++ js/main.js
@@ -1,51 +1,42 @@
-$(function(){
-	for (i=100 ;i>0 ; i--){
-		$(".works").append('<div class="work">' +
-								'<div class="pic" >'+
-									'<img src="images/day'+i+'.jpg" style="display:none">'+
-								'</div>'+
-								'<div class="day">'+
-									'<p>'+i+'</p>'+
-								'</div>'+
-								'<div class="loader">'+
-									'<div class="loader-inner cube-transition">'+
-										'<div></div>'+
-									'</div>'+
-								'</div>'+
-							'</div>');
+/* global $ */
+$(function () {
+  for (var i = 100; i > 0; i--) {
+    $('.works').append('<div class="work">' +
+                '<div class="pic" >' +
+                  '<img src="images/day' + i + '.jpg" style="display:none">' +
+                '</div>' +
+                '<div class="day">' +
+                  '<p>' + i + '</p>' +
+                '</div>' +
+                '<div class="loader">' +
+                  '<div class="loader-inner cube-transition">' +
+                    '<div></div>' +
+                  '</div>' +
+                '</div>' +
+              '</div>');
+  }
 
-	}
+  $('.pic img').load(function () {
+    $(this).show();
+    $(this).parent().parent().find('.loader').hide();
+  });
 
-	$(".pic img").load(function(){
-			console.log($(this).attr("src"));
-			console.log($($(this).parent()).attr('class'));
-			//console.log($(this).parent.attr('class'));
+  $('.pic img').error(function () {
+    $(this).parent().parent().hide();
+  });
 
-			$(this).show();
-			$(this).parent().parent().find(".loader").hide();
-	});
+  $.ajax({
+    url: 'links.json',
+    dataType: 'json'
+  })
+  .done(function (data) {
+    var linkHtml = '';
+    var linkTpl = '<div class="link ${name}"><a href="${url}" target="_blank"><i class="mdi mdi-${icon}"></i></a></div>';
 
-	$(".pic img").error(function(){
-			$(this).parent().parent().hide();
+    data.forEach(function (link) {
+      linkHtml += linkTpl.replace('${name}', link.name).replace('${url}', link.url).replace('${icon}', link.icon);
+    });
 
-	});
-
-		$.ajax({url:"links.json",
-				type: "GET", 
-				data: {},
-				cached: false,
-				dataType: "json",
-				success: function(data){
-						var linkHtml = '';
-						var linkTpl = '<div class="link ${name}"><a href="${url}" target="_blank"><i class="mdi mdi-${icon}"></i></a></div>';
-
-						data.forEach(function(link) {
-							linkHtml += linkTpl.replace('${name}', link.name).replace('${url}', link.url).replace('${icon}', link.icon);
-						});
-
-						$('.user .links').html(linkHtml);
-					}
-				});	
-
-
-})
+    $('.user .links').html(linkHtml);
+  });
+});
