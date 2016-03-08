3b33334388957271cc99b2472e0661d757be3e94
Rplus <rplus.tw@gmail.com>
2016-03-08 14:37:21 +0800

    調整 asset 載入時間點

--- js/main.js
+++ js/main.js
@@ -38,4 +38,8 @@ $(function () {
 
     $('.user .links').html(linkHtml);
   });
+
+  setTimeout(function () {
+    $('body').append('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/loaders.css/0.1.2/loaders.min.css"><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">');
+    }, 500);
 });
